c7caa60f3683feb8fd9ec8e9d53243373790e1e4
DannielF <danielgranados1992@gmail.com>
2022-04-21 16:40:15 -0500

    Test loop question

--- controller/index.mjs
+++ controller/index.mjs
@@ -1,59 +1,30 @@
 import history from '../model/history.js';
 import { tech, math, spanish, biology, culture } from '../model/questions.js';
 
-const startGame = () => {
-  const playerName = document.querySelector("#playerName").value;
-  setPlayer(playerName);
-  loopQuestions();
-}
+const mainGame = document.querySelector("#main-game");
+let isCorrect = false;
 
-const buttonStart = document.querySelector("#startGame");
-buttonStart.addEventListener("click", startGame);
-
-const setPlayer= (playerName) => {
-  history.push({player: playerName, points: 0});
-  const mainGame = document.querySelector("#main-game");
+const setPlayer = (playerName) => {
+  history.push({ player: playerName, points: 0 });
   const p = document.createElement("p");
   p.innerHTML = `Bienvenido ${history[0].player}`;
-  mainGame.append(p);
+  mainGame.appendChild(p);
 }
 
-const loopQuestions = () => {
-  const categories = [tech, math, spanish, biology, culture];
-  let i = 0;
-  let isLoop = true;
-  do {
-    showQuestions(categories[i]);
-    if (checkUserAnswer()) {
-      i++;
-    } else {
-      isLoop = false;
-      console.log("game over")
-    }
-  } while (isLoop);
-}
-
-const showQuestions = (category) => {
-  const mainGame = document.querySelector("#main-game");
+const showQuestions = (category, randomQuestion) => {
+  console.log("showQuestions");
   const question = document.createElement("p");
-  const randomQuestion = Math.floor((Math.random() * 5) + 1);
   question.innerHTML = "Pregunta: " + category[randomQuestion].question;
-  mainGame.append(question);
+  mainGame.appendChild(question);
   const answers = category[randomQuestion].answers;
 
   let i = 1;
   for (let a of answers) {
     const answer = document.createElement("li");
-    answer.innerHTML = `${i}`+ " : " + a;
-    mainGame.append(answer);
+    answer.innerHTML = `${i}` + " : " + a;
+    mainGame.appendChild(answer);
     i++;
   }
-
-  checkUserAnswer(category, randomQuestion);
-}
-
-function checkUserAnswer(category, randomQuestion) {
-  const mainSection = document.querySelector("#main-game");
 
   const inputUser = document.createElement("input");
   inputUser.setAttribute("id", "answerUser");
@@ -61,21 +32,65 @@ function checkUserAnswer(category, randomQuestion) {
   const buttonAnswer = document.createElement("button");
   buttonAnswer.setAttribute("id", "buttonAnswer");
   buttonAnswer.innerHTML = "Responder";
-  mainSection.append(inputUser, buttonAnswer);
-
-  buttonAnswer.addEventListener("click", (e) => {
-    const answerUser = document.querySelector("#answerUser").value.toLowerCase();
-    const answerCorrect = category[randomQuestion].correct.toLowerCase();
-    if (answerUser == answerCorrect) {
-      showPoints();
-      return true;
-    }
-  });
+  mainGame.append(inputUser, buttonAnswer);
 }
 
-const showPoints = () => {
-  const mainGame = document.querySelector("#main-game");
+function checkUserAnswer(category, randomQuestion, buttonAnswer) {
+  console.log("checkUserAnswer");
+
+  buttonAnswer.addEventListener("click", (e) => {
+    const answerUser = document.querySelector("#answerUser")
+      .value
+      .toLowerCase();
+    const answerCorrect = category[randomQuestion]
+      .correct
+      .toLowerCase();
+
+    if (answerUser == answerCorrect) {
+      isCorrect = true;
+      history[0].points++;
+      showPoints();
+    }
+
+    const p = document.createElement("p");
+    p.innerHTML = "Respuesta incorrecta";
+    mainGame.appendChild(p);
+
+  })
+  return isCorrect;
+}
+
+function showPoints() {
   const points = document.createElement("p");
   points.innerHTML = "Puntos: " + history[0].points;
   mainGame.append(points);
 }
+
+const loopQuestions = () => {
+  const categories = [tech, math, spanish, biology, culture];
+  let i = 0;
+  let category = categories[i];
+  let randomQuestion = Math.floor((Math.random() * 5) + 1);
+  
+  do {
+    showQuestions(category, randomQuestion);
+    let isLoop = checkUserAnswer(category, randomQuestion, buttonAnswer);
+    if (isLoop) {
+      console.log("next question");
+      i++;
+
+    } else {
+      isLoop = false;
+      console.log("game over")
+    }
+  } while (isLoop);
+}
+
+const startGame = () => {
+  const playerName = document.querySelector("#playerName").value;
+  setPlayer(playerName);
+  loopQuestions();
+}
+
+const buttonStart = document.querySelector("#startGame");
+buttonStart.addEventListener("click", startGame());
